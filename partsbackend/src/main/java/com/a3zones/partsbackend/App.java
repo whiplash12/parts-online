438ff2d7c34b795f757972dccdf1ead4a6668e90
whiplash12 <34149967+whiplash12@users.noreply.github.com>
2020-03-23 14:23:49 +0530

    My Project setup completed successfully.

---
+++ partsbackend/src/main/java/com/a3zones/partsbackend/App.java
@@ -0,0 +1,13 @@
+package com.a3zones.partsbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
